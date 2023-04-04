b1b957e9b92826f87d92e946a4fc3590f85f8cc1
MagicWinnie <magicwinnie@yandex.ru>
2023-04-04 21:41:41 +0300

    Added V type to graph

--- week_11/A.cpp
+++ week_11/A.cpp
@@ -1,37 +1,48 @@
 // Dmitriy Okoneshnikov
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-template <typename T>
+template <typename V, typename E>
 class Graph
 {
 private:
     size_t size;
-    vector<vector<T>> adj;
+    unordered_map<V, size_t> vertex_mapping;
+    vector<vector<E>> adj_matrix;
     vector<bool> visited;
     void DFS_worker(size_t v)
     {
         visited[v] = true;
         for (size_t u = 0; u < size; u++)
-            if (adj[v][u])
+            if (adj_matrix[v][u])
                 if (!visited[u])
                     DFS_worker(u);
+    }
+    void clear_visited()
+    {
+        for (size_t i = 0; i < size; i++)
+            visited[i] = false;
+    }
+    void DFS(size_t v)
+    {
+        clear_visited();
+        DFS_worker(v);
     }
 
 public:
     Graph(size_t n)
     {
         size = n;
-        adj.resize(size, vector<T>(size, T()));
+        adj_matrix.resize(size, vector<E>(size, E()));
         visited.resize(size, false);
     }
-    void DFS(size_t v)
+    void DFS(V v)
     {
-        for (size_t i = 0; i < size; i++)
-            visited[i] = false;
-        DFS_worker(v);
+        clear_visited();
+        DFS_worker(vertex_mapping[v]);
     }
     bool isEveryNodeReachable()
     {
@@ -41,24 +52,26 @@ public:
             for (size_t j = 0; j < visited.size(); j++)
                 if (!visited[j])
                     return false;
-            cout << endl;
         }
         return true;
     }
-    friend istream &operator>>(istream &in, Graph<T> &g)
+    friend istream &operator>>(istream &in, Graph<V, E> &g)
     {
         for (size_t i = 0; i < g.size; i++)
+        {
             for (size_t j = 0; j < g.size; j++)
-                in >> g.adj[i][j];
+                in >> g.adj_matrix[i][j];
+            g.vertex_mapping[i] = i;
+        }
         return in;
     }
-    friend ostream &operator<<(ostream &out, Graph<T> &g)
+    friend ostream &operator<<(ostream &out, Graph<V, E> &g)
     {
         for (size_t i = 0; i < g.size; i++)
         {
             for (size_t j = 0; j < g.size; j++)
             {
-                out << g.adj[i][j];
+                out << g.adj_matrix[i][j];
                 if (j != g.size - 1)
                     cout << ' ';
             }
@@ -73,7 +86,7 @@ int main(void)
 {
     int n;
     cin >> n;
-    Graph<int> g(n);
+    Graph<int, int> g(n); // not <int, bool> because we can't read into bool vector due to the features of the language
     cin >> g;
     cout << (g.isEveryNodeReachable() ? "YES" : "NO") << endl;
     return 0;
